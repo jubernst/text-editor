adf60c54aa95252510bda08a3c2292e44be95636
Julia B <julia.bernst@gmail.com>
2023-05-20 21:43:02 -0400

    Renamed text to jate

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,34 +1,34 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("text", 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("text")) {
-        console.log("text database already exists");
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore("text", { keyPath: "id", autoIncrement: true });
-      console.log("text database created");
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
-  const jateDB = await openDB("text", 1);
-  const tx = jateDB.transaction("text", "readwrite");
-  const store = tx.objectStore("text");
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.put({ text: content }); // assuming we don't need id
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("Data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from database");
-  const jateDB = await openDB("text", 1);
-  const tx = jateDB.transaction("text", "readonly");
-  const store = tx.objectStore("text");
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
   console.log("result.value", result);
